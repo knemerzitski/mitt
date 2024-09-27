da07837de1c966a96eda4e402871ab0b80192c83
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2024-09-27 21:11:07 +0300

    move mitt functions outside object

--- src/index.ts
+++ src/index.ts
@@ -51,73 +51,80 @@ export default function mitt<Events extends BaseEvents>(
 		| WildcardHandler<Events>;
 	all = all || new Map();
 
+	/**
+	 * Register an event handler for the given type.
+	 * @param {string|symbol} type Type of event to listen for, or `'*'` for all events
+	 * @param {Function} handler Function to call in response to given event
+	 * @memberOf mitt
+	 */
+	function on<Key extends keyof Events>(
+		type: Key,
+		handler: GenericEventHandler
+	) {
+		const handlers: Array<GenericEventHandler> | undefined = all!.get(type);
+		if (handlers) {
+			handlers.push(handler);
+		} else {
+			all!.set(type, [handler] as EventHandlerList<Events[keyof Events]>);
+		}
+	}
+
+	/**
+	 * Remove an event handler for the given type.
+	 * If `handler` is omitted, all handlers of the given type are removed.
+	 * @param {string|symbol} type Type of event to unregister `handler` from (`'*'` to remove a wildcard handler)
+	 * @param {Function} [handler] Handler function to remove
+	 * @memberOf mitt
+	 */
+	function off<Key extends keyof Events>(
+		type: Key,
+		handler?: GenericEventHandler
+	) {
+		const handlers: Array<GenericEventHandler> | undefined = all!.get(type);
+		if (handlers) {
+			if (handler) {
+				handlers.splice(handlers.indexOf(handler) >>> 0, 1);
+			} else {
+				all!.set(type, []);
+			}
+		}
+	}
+
+	/**
+	 * Invoke all handlers for the given type.
+	 * If present, `'*'` handlers are invoked after type-matched handlers.
+	 *
+	 * Note: Manually firing '*' handlers is not supported.
+	 *
+	 * @param {string|symbol} type The event type to invoke
+	 * @param {Any} [evt] Any value (object is recommended and powerful), passed to each handler
+	 * @memberOf mitt
+	 */
+	function emit<Key extends keyof Events>(type: Key, evt?: Events[Key]) {
+		let handlers = all!.get(type);
+		if (handlers) {
+			(handlers as EventHandlerList<Events[keyof Events]>)
+				.slice()
+				.map((handler) => {
+					handler(evt!);
+				});
+		}
+
+		handlers = all!.get('*');
+		if (handlers) {
+			(handlers as WildCardEventHandlerList<Events>).slice().map((handler) => {
+				handler(type, evt!);
+			});
+		}
+	}
+
 	return {
 		/**
 		 * A Map of event names to registered handler functions.
 		 */
 		all,
-
-		/**
-		 * Register an event handler for the given type.
-		 * @param {string|symbol} type Type of event to listen for, or `'*'` for all events
-		 * @param {Function} handler Function to call in response to given event
-		 * @memberOf mitt
-		 */
-		on<Key extends keyof Events>(type: Key, handler: GenericEventHandler) {
-			const handlers: Array<GenericEventHandler> | undefined = all!.get(type);
-			if (handlers) {
-				handlers.push(handler);
-			} else {
-				all!.set(type, [handler] as EventHandlerList<Events[keyof Events]>);
-			}
-		},
-
-		/**
-		 * Remove an event handler for the given type.
-		 * If `handler` is omitted, all handlers of the given type are removed.
-		 * @param {string|symbol} type Type of event to unregister `handler` from (`'*'` to remove a wildcard handler)
-		 * @param {Function} [handler] Handler function to remove
-		 * @memberOf mitt
-		 */
-		off<Key extends keyof Events>(type: Key, handler?: GenericEventHandler) {
-			const handlers: Array<GenericEventHandler> | undefined = all!.get(type);
-			if (handlers) {
-				if (handler) {
-					handlers.splice(handlers.indexOf(handler) >>> 0, 1);
-				} else {
-					all!.set(type, []);
-				}
-			}
-		},
-
-		/**
-		 * Invoke all handlers for the given type.
-		 * If present, `'*'` handlers are invoked after type-matched handlers.
-		 *
-		 * Note: Manually firing '*' handlers is not supported.
-		 *
-		 * @param {string|symbol} type The event type to invoke
-		 * @param {Any} [evt] Any value (object is recommended and powerful), passed to each handler
-		 * @memberOf mitt
-		 */
-		emit<Key extends keyof Events>(type: Key, evt?: Events[Key]) {
-			let handlers = all!.get(type);
-			if (handlers) {
-				(handlers as EventHandlerList<Events[keyof Events]>)
-					.slice()
-					.map((handler) => {
-						handler(evt!);
-					});
-			}
-
-			handlers = all!.get('*');
-			if (handlers) {
-				(handlers as WildCardEventHandlerList<Events>)
-					.slice()
-					.map((handler) => {
-						handler(type, evt!);
-					});
-			}
-		}
+		on,
+		off,
+		emit
 	};
 }
