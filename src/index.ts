cf6d0a8ff5904c5ae414b8aaf7a71058dead5ea6
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2024-09-27 23:39:53 +0300

    fix EmitterPickEvents

--- src/index.ts
+++ src/index.ts
@@ -20,12 +20,38 @@ export type EventHandlerMap<Events extends BaseEvents> = Map<
 	EventHandlerList<Events[keyof Events]> | WildCardEventHandlerList<Events>
 >;
 
-export type EmitterEvents<T> = T extends Emitter<infer R> ? R : never;
+export type EmitterEvents<T> = T extends Emitter<infer R>
+	? R
+	: T extends LimitedEmitter<infer R>
+	? R
+	: never;
 
 export type EmitterPickEvents<
-	T extends Emitter<any>,
+	T extends LimitedEmitter<any>,
 	P extends keyof EmitterEvents<T>
-> = Emitter<Pick<EmitterEvents<T>, P>>;
+> = LimitedEmitter<Pick<EmitterEvents<T>, P>>;
+
+interface LimitedEmitter<Events extends BaseEvents> {
+	on<Key extends keyof Events>(
+		type: Key,
+		handler: Handler<Events[Key]>
+	): () => void;
+
+	on<Key extends keyof Events>(
+		types: Key[],
+		handler: Handler<Events[Key]>
+	): () => void;
+
+	off<Key extends keyof Events>(
+		type: Key,
+		handler?: Handler<Events[Key]>
+	): void;
+
+	emit<Key extends keyof Events>(type: Key, event: Events[Key]): void;
+	emit<Key extends keyof Events>(
+		type: undefined extends Events[Key] ? Key : never
+	): void;
+}
 
 export interface Emitter<Events extends BaseEvents> {
 	all: EventHandlerMap<Events>;
